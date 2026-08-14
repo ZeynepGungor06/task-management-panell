82bd6c46a5f939a09ce47617a9a0156af7a3ae2e
ZeynepGungor06 <zeynepgungor06@gmail.com>
2026-08-14 13:19:07 +0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
         $token=$user->createToken('auth_token')->plainTextToken;
         return response()->json([
             'message'=>'Giriş başarılı',
-            'access _token'=>$token,
+            'access_token'=>$token,
             'token_type'=>'Bearer',
             'user'=>$user
         ]);
